0742d9f3c8e5326d07620794eda3cf6bc615f650
arasad1 <asaduzzamanaj07@gmail.com>
2023-03-31 10:47:42 +0600

    fetch data

---
+++ src/component/Home/Blogs.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BookmarkIcon } from '@heroicons/react/24/solid'
+
+const Blogs = ({ blog }) => {
+    const { person_name, person_img, slot, time, job_name, cover_img } = blog;
+    return (
+        <div className='p-4'>
+            <img className='rounded mb-6' src={cover_img} alt="" />
+            <div className='flex justify-between px-4 mb-3'>
+                <div className='flex gap-3'>
+                    <img className='w-12 h-12 rounded-full' src={person_img} alt="" />
+                    <div>
+                        <h3 className='font-bold'>{person_name}</h3>
+                        <p>{slot}</p>
+                    </div>
+                </div>
+                <div className='flex items-center'>
+                    <p>{time} min read</p>
+                    <button><BookmarkIcon className="h-6 w-6 " /></button>
+                </div>
+            </div>
+            <h1 className='text-3xl font-bold mb-3'>{job_name}</h1>
+            <button className='text-blue-500 text-lg font-semibold underline underline-offset-4 mb-4'>Mark as read</button>
+            <hr />
+        </div>
+
+    );
+};
+
+export default Blogs;
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Blogs from './Blogs';
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
+
     return (
         <div className='grid lg:grid grid-cols-3 gap-4'>
             <div className='col-span-2'>
-                <h2>This is bolgs container</h2>
+                {
+                    blogs.map(blog => <Blogs key={blog.id} blog={blog}></Blogs>)
+                }
             </div>
             <div>
                 <h2>This is cart container</h2>
